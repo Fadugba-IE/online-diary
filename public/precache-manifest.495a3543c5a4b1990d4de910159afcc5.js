3d73f24c31c437287f629c6ae15957067e002748
Iyanuoluwa <fadugba.iyanu@gmail.com>
2020-05-29 13:22:51 -0700

    debugging netlify issue

---
+++ public/precache-manifest.495a3543c5a4b1990d4de910159afcc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16cd95778986829e939e70d51c3079f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08fcdaaf9c5e01c2c8b3",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7a679345cd11b7485f51",
+    "url": "/static/css/main.e7e300ff.chunk.css"
+  },
+  {
+    "revision": "08fcdaaf9c5e01c2c8b3",
+    "url": "/static/js/2.d664438c.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/static/js/2.d664438c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a679345cd11b7485f51",
+    "url": "/static/js/main.2194613f.chunk.js"
+  },
+  {
+    "revision": "ebfbf02ae48ba56a24cd",
+    "url": "/static/js/runtime-main.0ccac30b.js"
+  },
+  {
+    "revision": "3b1b941cf7dbb32fcfae3d115eb2bce6",
+    "url": "/static/media/background.3b1b941c.jpeg"
+  }
+]);
