a87144dcfc53070f9d7c6434aecdfe54a09147ef
Iyanuoluwa <fadugba.iyanu@gmail.com>
2020-08-05 20:27:18 -0700

    Adjustments to dashboard

--- src/Components/DashboardForm.jsx
+++ src/Components/DashboardForm.jsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../App.css";
+import {Button} from "react-bootstrap";
+import history from "./history";
 
 
 function DashboardForm () {
@@ -31,21 +33,22 @@ function DashboardForm () {
         <h2>Create a Memory</h2>
 
         <form>
-                    <label>Date :</label>
+                 
                    <DatePicker className= "dateField"  selected = {memoryDate} onChange = {handleDateChange} dateFormat = "dd/MM/yyyy" />
 
-               <label>Memory :</label>
-                    <input onChange = {handleMemoryChange}
+               
+                    <textarea onChange = {handleMemoryChange}
+                      placeholder = "Write your memory here..."
                        name =  "memoryInput"
                     value = {memoryInput}
                    />
                    
 
-                   <input onClick = {handleSubmit}   type = "Submit"   value = "Create Memory" />
-
-               
+                   <input onClick = {handleSubmit}   type = "Submit"   defaultValue = "Create" />
+        
           
         </form>
+        <Button className='dashboard-button' variant="Primary" size="lg" onClick = {() => history.push('/login')}>Back to Login</Button>
         </div>
 
 
